03bb690de10d48bfdc6819ffe3c302593f86e745
James <lim.en.hui.james@gmail.com>
2018-03-03 09:19:22 +0800

    Created issue #5

---
+++ alerter/alerter.cpp
@@ -0,0 +1,77 @@
+#include "alerter.hpp"
+
+#include <sstream>
+
+Alerter::Alerter() {
+    HRESULT hr = MFStartup(MF_VERSION, MFSTARTUP_NOSOCKET); //starts up MF without sockets.
+    if (SUCCEEDED(hr)) {
+        alerterCloseEvent = CreateEvent(NULL, FALSE, FALSE, NULL); //not inheritble, auto-reset, no signal, nameless
+        if (alerterCloseEvent == NULL) {
+            std::stringstream ss;
+            ss << "Could not create event for use in alerter. HRESULT Error: " << HRESULT_FROM_WIN32(GetLastError());
+            throw std::runtime_error(std::string(ss.str().c_str()));
+        }
+    }
+}
+
+Alerter::~Alerter() {
+
+}
+
+AlertReturnCode Alerter::getLastError() {
+    return lastError;
+}
+
+void Alerter::setMode(AlerterMode am) {
+    a_mode = am;
+}
+
+HRESULT Alerter::CreatePlaybackTopology(IMFMediaSource* pSource, IMFPresentationDescriptor* pPD, IMFTopology **ppTopology) {
+    IMFTopologyPtr pTopology(NULL); //the topology we're generating
+    DWORD cSourceStreams = 0;
+
+    //Create topology
+    HRESULT hr = MFCreateTopology(&pTopology); 
+    
+}
+
+HRESULT Alerter::CreateMediaSource(PCWSTR sURL, IMFMediaSource **ppSource) {
+    MF_OBJECT_TYPE ObjectType = MF_OBJECT_INVALID; //object does not exist... yet
+    IMFSourceResolverPtr pSourceResolver(0); //interface that can resolve media sources from URL or byte stream
+    IUnknownPtr pSource(0); //base interface to most COM interfaces
+
+    /* Create the media source */
+    HRESULT hr = MFCreateSourceResolver(&pSourceResolver); //creates the source resolver
+
+    if (FAILED(hr))
+        return false;
+
+    /* Create the object */
+    hr = pSourceResolver->CreateObjectFromUrl(path.c_str(),
+                                            MF_RESOLUTION_MEDIASOURCE,
+                                            NULL,
+                                            &ObjectType,
+                                            &pSource);
+
+    if (FAILED(hr))
+        return false;
+
+    hr = pSource->QueryInterface(IDD_PPV_ARGS(ppSource)); //Get the IMFMediaSource interface from the media source.
+    return hr;
+}
+
+void Alerter::playAudio() {
+    switch (a_mode) {
+        case BACKGROUND:
+            HRESULT result = PlayAudioStream();
+            break;
+        case DUCKING:
+            lastError = UNSUPPORTED_OPERATION;
+            break;
+        case EXCLUSIVE
+            HRESULT result = PlayExclusiveStream();
+            break;
+        default:
+            throw std::logic_error("Unexpected case reached. Contact the application owner.");
+    }
+}
---
+++ alerter/alerter.hpp
@@ -0,0 +1,136 @@
+#ifndef ALERTER_H
+#define ALERTER_H
+
+#include <string> //string and wstring
+//#include <filesystem> //exists //enable again when GCC 8.0 is out
+
+#include "windows.h"
+#include "Mfidl.h"
+#include "mfapi.h"
+#include "comdef.h"
+#include "comip.h"
+#include "Unknwn.h"
+
+// Some typedefs for smart pointers
+_COM_SMARTPTR_TYPEDEF(IMFSourceResolver, __uuidof(IMFSourceResolver));
+_COM_SMARTPTR_TYPEDEF(IUnknown, IID_IUnknown);
+_COM_SMARTPTR_TYPEDEF(IMFTopology, IID_IMFTopology);
+_COM_SMARTPTR_TYPEDEF(IMFPresentationDescriptor, IID_IMFPresentationDescriptor);
+_COM_SMARTPTR_TYPEDEF(IMFMediaSource, IID_IMFMediaSource);
+
+enum AlerterMode : short {
+    BACKGROUND,
+    DUCKING,
+    EXCLUSIVE
+};
+
+enum AlertReturnCode : short {
+    SUCCESS,
+    FILE_NOT_FOUND,
+    UNSUPPORTED_OPERATION
+};
+
+/**
+ * The alerter (sound) class. This class handles the audio.
+ * 
+ * This class will eventually support 3 different modes: background, ducking, and exclusive.
+ * In background mode, the sound will play like a normal application, and will not attempt to mute all other sound.
+ * In ducking mode, the sound will use a communications stream to play the sound, and the behaviour is defined by the user in the Windows Sound Settings.
+ * In exclusive mode, the sound will take exclusive control of the default audio device, muting all other volumes to alert the user.
+ * The fallback mode will always be background mode.
+**/
+class Alerter {
+    public:
+    Alerter();
+    virtual ~Alerter();
+
+    /* Error Handling */
+    AlertReturnCode getLastError(); //allows the user to obtain the last error
+
+    /* Mode setting */
+    void setMode(AlerterMode am); //sets the mode of the alerter
+
+    /* Sound file */
+    template <typename S>
+    bool setSoundFile(S path) { //underlying code uses a source resolver, so this will take a while.
+        return setSoundFile(std::wstring(path.begin(), path.end()));
+    }
+    template <typename S>
+    bool setSoundFile_Async(); //TODO: Actually do this later
+
+    /* Unified Function to play audio */
+    void playAudio();
+
+    protected:
+    HRESULT PlayAudioStream(); //background
+    //TODO: Ducking Mode
+    HRESULT PlayExclusiveStream(); //exclusive
+
+    // So according to the MSDN docs, I need to create my own partial network function.
+    // IGN 10/10
+    HRESULT CreatePlaybackTopology(IMFMediaSource* pSource, IMFPresentationDescriptor* pPD, IMFTopology **ppTopology);
+    HRESULT CreateMediaSource(PCWSTR sURL, IMFMediaSource **ppSource);
+
+    private:
+    AlerterMode a_mode = BACKGROUND; //the mode of the alerter
+    AlertReturnCode lastError = SUCCESS; //the last error that the object encountered
+    HANDLE alerterCloseEvent = NULL; //the handle that determines if things should stop
+
+    IMFMediaSourcePtr m_pSource(0);
+};
+
+template <>
+bool Alerter::setSoundFile<std::wstring>(std::wstring path) {
+    /* Check if the file exists */ //uncomment in GCC 8.0
+    /*if (!std::filesystem::exists(path)) {
+        lastError = FILE_NOT_FOUND; //sets the last error
+        return false; //function failed
+    }*/
+    if (FILE *file = fopen(std::string(path.begin(), path.end()).c_str(), "r")) { //checks for the existance of the file
+        fclose(file);
+    } else {
+        lastError = FILE_NOT_FOUND; //sets the last error
+        return false;
+    }
+
+    /* Variable Declaration */
+    IMFTopologyPtr pTopology(0); //the topoogy (it's a thing that describe the path of the media source to the media sink through nodes)
+    IMFPresentationDescriptorPtr pSourcePD(0); //the presentation descripter
+
+    /* Create a Media Session */
+    HRESULT hr = NULL;
+    //HRESULT hr = CreateSession();
+    if (FAILED(hr)) //failed just checks if hr < 0
+        return false;
+
+    /* Source Resolving */
+    hr = MFCreateSourceResolver(&pSourceResolver); //creates the source resolver
+
+    if (FAILED(hr))
+        return false;
+
+    /* Creating the object */
+    hr = pSourceResolver->CreateObjectFromUrl(path.c_str(),
+                                            MF_RESOLUTION_MEDIASOURCE,
+                                            NULL,
+                                            &ObjectType,
+                                            &pSource);
+    
+    if (FAILED(hr))
+        return false;
+
+    /* Creating the media source from the object */
+    hr = CreateMediaSource(path.c_str(), &pSource);
+    if (FAILED(hr))
+        return false;
+
+    /* Create the presentation descriptor */
+    hr = pSource->CreatePresentationDescriptor(&pSourcePD);
+    if (FAILED(hr))
+        return false;
+
+    /* Create a partial topology (I don't have the complete nodes) */
+    //hr = 
+}
+
+#endif
